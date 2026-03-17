bf4de70d9d781b9e6d3c2c1b6461602f0500c964
rajwadkar-riddhesh <rajwadkar.riddhesh@fyntune.com>
2026-03-17 16:18:43 +0530

    feat: Added CSS style for the keys and trasistion

--- Drum-Kit/src/main.jsx
+++ Drum-Kit/src/main.jsx
@@ -27,44 +27,44 @@ function App() {
 return(
   <>
 
-    <div className="keys">
-      <div data-key="65" className="key">
-        <kbd>A</kbd>
-        <span className="sound">clap</span>
-      </div>
-      <div data-key="83" className="key">
-        <kbd>S</kbd>
-        <span className="sound">hihat</span>
-      </div>
-      <div data-key="68" className="key">
-        <kbd>D</kbd>
-        <span className="sound">kick</span>
-      </div>
-      <div data-key="70" className="key">
-        <kbd>F</kbd>
-        <span className="sound">openhat</span>
-      </div>
-      <div data-key="71" className="key">
-        <kbd>G</kbd>
-        <span className="sound">boom</span>
-      </div>
-      <div data-key="72" className="key">
-        <kbd>H</kbd>
-        <span className="sound">ride</span>
-      </div>
-      <div data-key="74" className="key">
-        <kbd>J</kbd>
-        <span className="sound">snare</span>
-      </div>
-      <div data-key="75" className="key">
-        <kbd>K</kbd>
-        <span className="sound">tom</span>
-      </div>
-      <div data-key="76" className="key">
-        <kbd>L</kbd>
-        <span className="sound">tink</span>
-      </div>
+    <div class="keys">
+    <div data-key="65" class="key">
+      <kbd>A</kbd>
+      <span class="sound">clap</span>
     </div>
+    <div data-key="83" class="key">
+      <kbd>S</kbd>
+      <span class="sound">hihat</span>
+    </div>
+    <div data-key="68" class="key">
+      <kbd>D</kbd>
+      <span class="sound">kick</span>
+    </div>
+    <div data-key="70" class="key">
+      <kbd>F</kbd>
+      <span class="sound">openhat</span>
+    </div>
+    <div data-key="71" class="key">
+      <kbd>G</kbd>
+      <span class="sound">boom</span>
+    </div>
+    <div data-key="72" class="key">
+      <kbd>H</kbd>
+      <span class="sound">ride</span>
+    </div>
+    <div data-key="74" class="key">
+      <kbd>J</kbd>
+      <span class="sound">snare</span>
+    </div>
+    <div data-key="75" class="key">
+      <kbd>K</kbd>
+      <span class="sound">tom</span>
+    </div>
+    <div data-key="76" class="key">
+      <kbd>L</kbd>
+      <span class="sound">tink</span>
+    </div>
+  </div>
 
     <div>
       <audio data-key="65" src="/sounds/clap.wav"></audio>
